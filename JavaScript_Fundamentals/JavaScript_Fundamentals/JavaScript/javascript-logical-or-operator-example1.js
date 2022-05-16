f770416419a0abd9b24f4339578df63f89755c9f
unknown <mangesh.purndare@ics-global.in>
2022-05-16 11:39:16 +0530

    Adding project for Javascript and XML

---
+++ JavaScript_Fundamentals/JavaScript_Fundamentals/JavaScript/javascript-logical-or-operator-example1.js
@@ -0,0 +1,16 @@
+﻿function viewOutput() {
+    'use strict';
+    var no = document.getElementById('no').value;
+    if (no < 5 || no > 20) {
+        var newParagraph = document.createElement("p"); //creates a new paragraph element
+        var newText = document.createTextNode('Value of input is less than 5 or greater than 20'); //creates text along with ouput to be displayed 
+        newParagraph.appendChild(newText); //created text is appended to the paragraph element created
+        document.body.appendChild(newParagraph); // created paragraph and text along with output is appended to the document body
+    }
+    else {
+        var newParagraph1 = document.createElement("p"); //creates a new paragraph element
+        var newText1 = document.createTextNode('Wrong input'); //creates text along with ouput to be displayed 
+        newParagraph1.appendChild(newText1); //created text is appended to the paragraph element created
+        document.body.appendChild(newParagraph1); // created paragraph and text along with output is appended to the document body
+    }
+}
---
+++ JavaScript_Fundamentals/JavaScript_Fundamentals/JavaScript/subtraction-example1.js
@@ -0,0 +1,19 @@
+﻿var var1 = 45;
+var var2 = 78;
+var str1 = "Mangesh";
+var str2 = "Purandare";
+var newvar = var1 - var2;
+var newstr = str1 - str2;
+var varstr = var1 - str2;
+var newParagraph = document.createElement("p");
+var newText = document.createTextNode("var1 -var2 = " + newvar);
+newParagraph.appendChild(newText);
+document.body.appendChild(newParagraph);
+var newParagraph1 = document.createElement("p");
+var newText1 = document.createTextNode("str1 - str2  = " + newstr);
+newParagraph1.appendChild(newText1);
+document.body.appendChild(newParagraph1);
+var newParagraph2 = document.createElement("p");
+var newText2 = document.createTextNode("var1 - str2  = " + varstr);
+newParagraph2.appendChild(newText2);
+document.body.appendChild(newParagraph2)
